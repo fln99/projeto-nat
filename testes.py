431cd0d923621579e1309d0d38e6011592c23cfa
Felipe Nunes <59563220+fln99@users.noreply.github.com>
2022-03-31 20:32:27 -0300

    first commit

---
+++ testes.py
@@ -0,0 +1,55 @@
+'''
+Protótipo: Máquina de Refrigerante
+
+Componentes do grupo:
+- Olivia
+- Guilherme
+- Felipe
+'''
+
+while True:
+    garrafas_refri = int(input("Quantidade de garrafas de refrigerante: "))
+
+    if garrafas_refri > 10:
+        print("Limite máximo permitido de 10 litros!")
+        continue
+
+    break
+
+capacidade_garrafas = float(input("Capacidade das garrafas (litros): "))
+
+capacidade_copos = float(input("Capacidade dos copos (mililitros): "))
+
+while True:
+    total_pessoas = int(input("Quantidade de pessoas no evento: "))
+
+    if total_pessoas > 200:
+        print("Limite máximo de 200 pessoas!")
+        continue
+
+    break
+
+
+# print(f'{garrafas_refri} {capacidade_garrafas} {capacidade_copos} {total_pessoas}')
+
+total_litros_refri = garrafas_refri * capacidade_garrafas
+quantidade_copos = (total_litros_refri * 1000) / capacidade_copos
+copos_faltantes = total_pessoas - quantidade_copos
+
+print(f"[teste] Litros de refrigerante: {garrafas_refri * capacidade_garrafas}")
+
+print('-' * 20)
+
+print(f"{round(quantidade_copos,2)} copos de {capacidade_copos} ml")
+
+palavra_chave = ''
+
+if copos_faltantes < 0:
+    palavra_chave = 'a mais'
+else:
+    palavra_chave = 'faltantes'
+
+print(f"{abs(round(copos_faltantes, 2))} copos {palavra_chave}.")
+
+print(f"{abs(round((copos_faltantes * capacidade_copos) / 1000))} litros {palavra_chave}")
+
